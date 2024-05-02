e229a37350d91d9f0bd883ebfec5796e60044810
Utkarsh Tiwari <utkarsh.tiwari@tricog.com>
2024-05-02 15:22:22 +0530

    Complete image upload

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,16 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return (
+        <img 
+          src={`https://ut-blogster-bucket.s3.ap-south-1.amazonaws.com/${this.props.blog.imageUrl}`}
+        />
+      )
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +28,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
